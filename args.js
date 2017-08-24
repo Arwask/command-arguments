8ed3791f24dab272abd0862e9c9c283c72cc0669
Arwa <arwa.it15@gmail.com>
2017-08-24 15:06:06 -0500

    print 0 if no args

--- args.js
+++ args.js
@@ -3,9 +3,14 @@
 let args = process.argv;
 
 let [,,...numbers] = args;
-let answer = numbers.reduce( (num1, num2) => {
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    return num1+num2;
-})
-console.log(answer);
+if(numbers.length === 0)
+    console.log('0');
+else {
+    let answer = numbers.reduce( (num1, num2) => {
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        return num1+num2;
+    })
+    console.log(answer);
+    
+}
