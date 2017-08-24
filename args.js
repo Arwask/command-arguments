63a3fdc8406a7e8d9e79adf404f29a36090d3361
Arwa <arwa.it15@gmail.com>
2017-08-24 15:08:42 -0500

    use array.map

--- args.js
+++ args.js
@@ -3,12 +3,15 @@
 let args = process.argv;
 
 let [,,...numbers] = args;
+numbers = numbers.map( (num) => {
+    return parseFloat(num);
+})
 if(numbers.length === 0)
     console.log('0');
 else {
     let answer = numbers.reduce( (num1, num2) => {
-        num1 = parseFloat(num1);
-        num2 = parseFloat(num2);
+        // num1 = parseFloat(num1);
+        // num2 = parseFloat(num2);
         return num1+num2;
     })
     console.log(answer);
